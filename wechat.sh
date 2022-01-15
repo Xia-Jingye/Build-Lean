09ebf1ea02101bab04f7c4208f82db1e2f56f1c1
Xia-Jingye <83964074+Xia-Jingye@users.noreply.github.com>
2022-01-16 05:19:28 +0800

    Update wechat.sh

--- wechat.sh
+++ wechat.sh
@@ -40,7 +40,7 @@ IFSUCCESS="success"
 
 if [[ ${IFSUCCESS} == success ]]; then
     echo -e "${INFO} Sending message to WeChat..."
-    curl -s -X POST -H "Content-Type: application/json" -d '{"token":"'"$TOKEN"'", "title":"Lean固件编译成功", "content":"下载地址", "channel":"'"$CHANNEL"'", "webhook":"'"$WEBHOOK"'"}' http://www.pushplus.plus/send >${WECHAT_LOG}
+    curl -s -X POST -H "Content-Type: application/json" -d '{"token":"'"$PUSHPLUSTOKEN"'", "title":"Lean固件编译成功", "content":"下载地址", "channel":"'"$CHANNEL"'", "webhook":"'"$PUSHPLUSWEBHOOK"'"}' http://www.pushplus.plus/send >${WECHAT_LOG}
     PUSHPLUSSTATUS=$(echo ${WECHAT_LOG} | grep -q '"code":200')
     if [[ ${PUSHPLUSSTATUS} != 0 ]]; then
         echo -e "${ERROR} WeChat message sending failed: $(cat ${WECHAT_LOG})"
@@ -49,7 +49,7 @@ if [[ ${IFSUCCESS} == success ]]; then
     fi
 else
     echo -e "${INFO} Sending message to WeChat..."
-    curl -s -X POST -H "Content-Type: application/json" -d '{"token":"'"$TOKEN"'", "title":"Lean固件编译失败", "content":"下载地址", "channel":"'"$CHANNEL"'", "webhook":"'"$WEBHOOK"'"}' http://www.pushplus.plus/send >${WECHAT_LOG}
+    curl -s -X POST -H "Content-Type: application/json" -d '{"token":"'"$PUSHPLUSTOKEN"'", "title":"Lean固件编译失败", "content":"下载地址", "channel":"'"$CHANNEL"'", "webhook":"'"$PUSHPLUSWEBHOOK"'"}' http://www.pushplus.plus/send >${WECHAT_LOG}
     PUSHPLUSSTATUS=$(echo ${WECHAT_LOG} | grep -q '"code":200')
     if [[ ${PUSHPLUSSTATUS} != 0 ]]; then
         echo -e "${ERROR} WeChat message sending failed: $(cat ${WECHAT_LOG})"
