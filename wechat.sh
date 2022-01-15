2cb01ef720f27e9906f430362dc895e1cb580b8f
Xia-Jingye <83964074+Xia-Jingye@users.noreply.github.com>
2022-01-16 01:34:13 +0800

    Update wechat.sh

--- wechat.sh
+++ wechat.sh
@@ -9,21 +9,49 @@ Red_background_prefix="\033[41;37m"
 Font_color_suffix="\033[0m"
 INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
 ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
+TOKEN="a0fb7c9e4cc240a784bc5af9c377db51"
+WEBHOOK="xiajingye"
+TITLESUCCESS="固件编译成功"
+TITLEFAIL="固件编译失败"
+#CONTENT="${DOWNURL}"
+CHANNEL="cp"
+PUSHPLUSPOSTSUCCESS='{"token":"'${TOKEN}'", "title":"'${TITLESUCCESS}'", "content":"'${DOWNURL}'", "channel":"'${CHANNEL}'", "webhook":"'${WEBHOOK}'"}'
+PUSHPLUSPOSTFAIL='{"token":"'${TOKEN}'", "title":"'${TITLEFAIL}'", "content":"失败了哪还有下载地址。", "channel":"'${CHANNEL}'", "webhook":"'${WEBHOOK}'"}'
+
+#if [[ ${IFSUCCESS} == success ]]; then
+#    echo -e "${INFO} Sending message to WeChat..."
+#    curl -s "https://sc.ftqq.com/${SCKEY}.send?text=Lean固件编译成功。" -d "&desp=${DOWNURL}" >${WECHAT_LOG}
+#    WECHAT_STATUS=$(cat ${WECHAT_LOG} | jq -r .errmsg)
+#    if [[ ${WECHAT_STATUS} != success ]]; then
+#        echo -e "${ERROR} WeChat message sending failed: $(cat ${WECHAT_LOG})"
+#    else
+#        echo -e "${INFO} WeChat message sent successfully!"
+#    fi
+#else
+#    echo -e "${INFO} Sending message to WeChat..."
+#    curl -s "https://sc.ftqq.com/${SCKEY}.send?text=Lean固件编译失败。" -d "&desp=失败了哪还有下载地址。" >${WECHAT_LOG}
+#    WECHAT_STATUS=$(cat ${WECHAT_LOG} | jq -r .errmsg)
+#    if [[ ${WECHAT_STATUS} != success ]]; then
+#        echo -e "${ERROR} WeChat message sending failed: $(cat ${WECHAT_LOG})"
+#    else
+#        echo -e "${INFO} WeChat message sent successfully!"
+#    fi
+#fi
 
 if [[ ${IFSUCCESS} == success ]]; then
     echo -e "${INFO} Sending message to WeChat..."
-    curl -s "https://sc.ftqq.com/${SCKEY}.send?text=Lean固件编译成功。" -d "&desp=${DOWNURL}" >${WECHAT_LOG}
-    WECHAT_STATUS=$(cat ${WECHAT_LOG} | jq -r .errmsg)
-    if [[ ${WECHAT_STATUS} != success ]]; then
+    curl -s -X POST -H "Content-Type: application/json" -d "${PUSHPLUSPOSTSUCCESS}" http://www.pushplus.plus/send >${WECHAT_LOG}
+    PUSHPLUSSTATUS=$(echo ${WECHAT_LOG} | grep -q '"code":200')
+    if [[ ${PUSHPLUSSTATUS} != 0 ]]; then
         echo -e "${ERROR} WeChat message sending failed: $(cat ${WECHAT_LOG})"
     else
         echo -e "${INFO} WeChat message sent successfully!"
     fi
 else
     echo -e "${INFO} Sending message to WeChat..."
-    curl -s "https://sc.ftqq.com/${SCKEY}.send?text=Lean固件编译失败。" -d "&desp=失败了哪还有下载地址。" >${WECHAT_LOG}
-    WECHAT_STATUS=$(cat ${WECHAT_LOG} | jq -r .errmsg)
-    if [[ ${WECHAT_STATUS} != success ]]; then
+    curl -s -X POST -H "Content-Type: application/json" -d "${PUSHPLUSPOSTFAIL}" http://www.pushplus.plus/send >${WECHAT_LOG}
+    PUSHPLUSSTATUS=$(echo ${WECHAT_LOG} | grep -q '"code":200')
+    if [[ ${PUSHPLUSSTATUS} != 0 ]]; then
         echo -e "${ERROR} WeChat message sending failed: $(cat ${WECHAT_LOG})"
     else
         echo -e "${INFO} WeChat message sent successfully!"
